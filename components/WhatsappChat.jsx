6a452970e914cf24fb4b6582240642958c1e5d67
Sameer Rifat <sameerrifatbrw789@gmail.com>
2023-12-25 00:14:37 +0500

    2nd

--- components/WhatsappChat.jsx
+++ components/WhatsappChat.jsx
@@ -7,7 +7,7 @@ const WhatsappChat = () => {
     return (
         <div className='fixed right-5 bottom-5'>
             <Link
-                href={`https://wa.me/${923417082407}`}
+                href={`https://wa.me/${971557924200}`}
                 target='_blank'
                 className='bg-green-500 p-3 rounded-full flex justify-center items-center shadow-lg hover:bg-green-600 hover:shadow-lg hover:scale-110 transition-all'
             >
