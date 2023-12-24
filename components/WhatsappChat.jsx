a80e24dbb195272e3633e9d41e7adeb10e61cd3b
Sameer Rifat <sameerrifatbrw789@gmail.com>
2023-12-25 03:14:41 +0500

    4rth

--- components/WhatsappChat.jsx
+++ components/WhatsappChat.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import PhoneForwardedIcon from '@mui/icons-material/PhoneForwarded';
 import Link from 'next/link';
 
 
@@ -21,14 +22,14 @@ const WhatsappChat = () => {
                     <WhatsAppIcon className='text-white' fontSize='large' />
                 </Link>
             </div>
-            <div className='fixed right-6 bottom-32'>
+            <div className='fixed right-[22px] bottom-32'>
                 <Link
                     href={'tel:+971557924200'}
                     target='_blank'
                     rel="noopener noreferrer"
-                    className='bg-blue-500 p-2 rounded-md flex justify-center items-center shadow-lg hover:bg-blue-600 hover:shadow-lg hover:scale-110 transition-all'
+                    className='bg-blue-500 p-3 rounded-full flex justify-center items-center shadow-lg hover:shadow-xl hover:scale-110 transition-all'
                 >
-                    <PhoneInTalkIcon className='text-white' fontSize='medium' />
+                    <PhoneForwardedIcon className='text-white' fontSize='medium' />
                 </Link>
             </div>
         </>
