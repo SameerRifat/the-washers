68c8b918c24d0024bf609f298f81af1c39d5381f
Sameer Rifat <sameerrifatbrw789@gmail.com>
2024-01-03 04:19:39 +0500

    new

--- components/CleaningMethods.jsx
+++ components/CleaningMethods.jsx
@@ -40,7 +40,7 @@ const CleaningMethods = () => {
                         return (
                             <div key={ind} className='flex items-center gap-5'>
                                 <div className='bg-white p-3 xs:p-3 rounded-lg shadow-sm'>
-                                    <AutorenewIcon className=' text-green-500' style={isSmallScreen ? {fontSize: '20px'} : {fontSize: '50px'}}/>
+                                    <AutorenewIcon className=' text-green-500' style={isSmallScreen ? {fontSize: '18px'} : {fontSize: '50px'}}/>
                                 </div>
                                 <div>
                                     <h3 className='font-semibold xs:font-bold text-xl xs:text-2xl'>{method.title}</h3>
