47706b5dd1563e1e8eed8d1d3563bea508465f50
Sameer Rifat <sameerrifatbrw789@gmail.com>
2024-01-03 06:33:05 +0500

    chage metadata

--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'TheWashers UAE | Premier Cleaning Services in Dubai',
-  description: 'Discover unrivaled cleaning services at TheWashers UAE. Our expert team ensures pristine cleanliness in [Your City] homes and businesses. Schedule your appointment for a spotless space today!',
+  title: 'TheWashers UAE | Sofa Cleaning Services in Dubai',
+  description: 'Get top-notch cleaning services at TheWashers UAE! Our expert team guarantees spotless and pristine results for your home or business in Dubai, Sharjah, and Ajman. Book your appointment today and enjoy a sparkling clean space!',
 }
 // export const metadata = {
 //   title: 'thewashersuae - cleaning service in Dubai',
